a3660dc0717fc41f91adb140f2ef5093aebc1ef7
Mario Thompson <MarioThompson0010@gmail.com>
2021-02-14 16:32:23 -0600

    deploying to heroku

---
+++ client/build/precache-manifest.eb6e304ca0d749970e661f34ddcf58b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf7bc22c1230e60ff03efce9878b869f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbffa8540779d29ed20c",
+    "url": "/static/css/main.e06d672a.chunk.css"
+  },
+  {
+    "revision": "671b24dc39c3a43559d8",
+    "url": "/static/js/2.eacef7c6.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.eacef7c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbffa8540779d29ed20c",
+    "url": "/static/js/main.09b4ef88.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  }
+]);
